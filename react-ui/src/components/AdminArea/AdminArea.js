0a7c45480d938aa43f2d282babb6dd2b64258d3a
mtomic88 <mtomic@itekako.com>
2018-06-11 15:28:18 +0200

    basic routing for react and express

---
+++ react-ui/src/components/AdminArea/AdminArea.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class AdminArea extends Component {
+  render() {
+    return (
+      <div>Hello AdminArea!</div>
+    );
+  }
+}
+
+export default AdminArea;
