9f374251aad693fc61d101c7ebde6dd367352468
mtomic88 <mtomic@itekako.com>
2018-06-11 16:09:17 +0200

    creating store with redux, adding missing picture etc

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -3,19 +3,19 @@ import {
   BrowserRouter as Router,
   Route
 } from 'react-router-dom';
-import MainArea from './components/MainArea/MainArea';
-import AdminArea from './components/AdminArea/AdminArea';
+import MainArea from './features/MainArea/MainAreaContainer';
+import AdminArea from './features/AdminArea/AdminAreaContainer';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <Router>
-        <div>
-          <Route exact path="/" component={MainArea}/>
-          <Route exact path="/admin" component={AdminArea}/>
-        </div>
-      </Router>
+        <Router>
+          <div>
+            <Route exact path="/" component={MainArea}/>
+            <Route exact path="/admin" component={AdminArea}/>
+          </div>
+        </Router>
     );
   }
 }
--- react-ui/src/index.js
+++ react-ui/src/index.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import './index.css';
 import App from './App';
+import configureStore from './configureStore';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const root = <Provider store={configureStore()}>
+  <MuiThemeProvider>
+    <App />
+  </MuiThemeProvider>
+</Provider>;
+
+ReactDOM.render(root, document.getElementById('root'));
 registerServiceWorker();
